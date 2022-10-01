32bad0167cee0288ba98aa84ca7020353afae83c
zhangxiaolong <2254297439@qq.com>
2022-10-01 20:41:28 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author zhangxiaolong
+ * @date 2022/10/1-20:37
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!!!");
+    }
+}
